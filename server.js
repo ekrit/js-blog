acf021d935c5df7c6dce3438efbdc579c27b8adb
haris.tiric <haris.harisaga@gmail.com>
2024-03-08 11:45:41 +0100

    w

--- server.js
+++ server.js
@@ -1,7 +1,54 @@
 const http = require('http');
+const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req, res)=>{
-    console.log("Request made")
+    console.log(req.url, req.method)
+
+    res.setHeader('Content-Type', 'text/html');
+
+    let path = './views/'
+
+    const random = _.random(0,20);
+    console.log("random broj: " + random);
+
+    const greet = _.once(()=>{
+        console.log("Helllo")
+    })
+
+    greet()
+    greet()
+
+
+    switch(req.url){
+        case '/':
+            path += 'index.html'
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html'
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default:
+            path += '404.html'
+            res.statusCode = 404;
+            break;
+    }
+
+    fs.readFile(path, (err, data) => {
+        if(err){
+            console.log(err);
+            res.end()
+        }
+        else{
+            res.end(data);
+        }
+    })
 });
 
 server.listen(3000, 'localhost', ()=>{
